caa5bfd62892bb43af1994655e7c145b4219511d
klingern <klingern@GC127A-10.securebucks.edu>
2019-09-04 15:10:43 -0400

    Changed functions

--- Lab 1/src/lab1.java
+++ Lab 1/src/lab1.java
@@ -2,9 +2,11 @@
 public class lab1 {
 
 	public static void main(String[] args) {
-		System.out.println(implication());
-		System.out.println(equality());
-		System.out.println(disjunction());
+		//System.out.println(implication());
+		//System.out.println(equality());
+		//System.out.println(disjunction());
+		//System.out.println(NAND());
+		System.out.println(NOR());
 	}
 
 	public static boolean implication() {
@@ -35,11 +37,37 @@ public class lab1 {
 		int x = 7;
 		int s = 8;
 		if (x == s) {
-			System.out.println("The statement is equal");
+			System.out.println("The statement is equal, which is not a disjunction");
 			return x == s;
 		} else {
 			System.out.println("The statement is not equal, also known as an Exclusive disjunction");
 			return x == s;
 		}
+	}
+	public static boolean NAND() {
+		boolean x = true;
+		boolean y = true;
+		
+		if (!(x && y)) {
+			System.out.print("The NAND is ");
+			return (!(x&&y));
+		}
+		else {
+			System.out.print("The NAND is ");
+		return (!(x&&y));
+		}
+	}
+		public static boolean NOR() {
+			boolean x = false;
+			boolean y = false;
+			
+			if (!(x || y)) {
+				System.out.print("The NOR is ");
+				return (!(x || y));
+			}
+			else {
+				System.out.print("The NOR is ");
+			return (!(x || y));
+			}
 	}
 }
