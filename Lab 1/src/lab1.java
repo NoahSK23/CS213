f833eff690513f3c51ca4afcd4c326588519a7aa
NoahSK <noahsklinger@gmail.com>
2019-09-09 13:48:53 -0400

    All functions work; might add a scanner feature later

--- Lab 1/src/lab1.java
+++ Lab 1/src/lab1.java
@@ -2,29 +2,34 @@
 public class lab1 {
 
 	public static void main(String[] args) {
-		//System.out.println(implication());
-		//System.out.println(equality());
-		//System.out.println(disjunction());
-		//System.out.println(NAND());
+		System.out.println(implication());
+		System.out.println(equality());
+		System.out.println(disjunction());
+		System.out.println(NAND());
 		System.out.println(NOR());
 	}
 
 	public static boolean implication() {
-		int a = 5;
-		int b = 5;
-		int c = 10;
-		if (a + b == c) {
-			System.out.print("The implication is ");
+		boolean a = true;
+		boolean b = false;
+		if (b == false) {
+			System.out
+					.print("The value a is " + a + " and the value b is " + b + " which is not a logical "
+							+ "implication since b is " + b + ": ");
+		return false;
 		}
-		return a + b == c;
+		else {
+			System.out.print("The value a is " + a + " and the value b is " + b + " which is a logical implication: ");
+			return true;
+		}
 	}
 
 	public static boolean equality() {
 
-		int x = 7;
-		int s = 7;
+		boolean x = false;
+		boolean s = false;
 		if (x == s) {
-			System.out.println("The statement is equal because " + x + " is equal to " + s);
+			System.out.print("The statement is equal because " + x + " is equal to " + s + ". Logical equality: ");
 			return x == s;
 		} else {
 			System.out.println("The statement is not equal because " + x + " is not equal to " + s);
@@ -34,40 +39,41 @@ public class lab1 {
 
 	public static boolean disjunction() {
 
-		int x = 7;
-		int s = 8;
-		if (x == s) {
-			System.out.println("The statement is equal, which is not a disjunction");
-			return x == s;
+		boolean x = true;
+		boolean s = true;
+		if (x != s) {
+			System.out.print("x is " + x + " and s is " + s + ", so the disjunction is: ");
+			return x || s;
 		} else {
-			System.out.println("The statement is not equal, also known as an Exclusive disjunction");
-			return x == s;
+			System.out.print("x is " + x + " and s is " + s + ", so the disjunction is: ");
+			return x || s;
 		}
 	}
+
 	public static boolean NAND() {
 		boolean x = true;
 		boolean y = true;
-		
+
 		if (!(x && y)) {
-			System.out.print("The NAND is ");
-			return (!(x&&y));
-		}
-		else {
-			System.out.print("The NAND is ");
-		return (!(x&&y));
+			System.out.print("Since value x and y are not both true, the NAND is: ");
+			return (!(x && y));
+		} else {
+			System.out.print("The x and y values are both true, which makes a NAND: ");
+			return (!(x && y));
 		}
 	}
-		public static boolean NOR() {
-			boolean x = false;
-			boolean y = false;
-			
-			if (!(x || y)) {
-				System.out.print("The NOR is ");
-				return (!(x || y));
-			}
-			else {
-				System.out.print("The NOR is ");
+
+	public static boolean NOR() {
+		boolean x = true;
+		boolean y = false;
+
+		if (!(x || y)) {
+			System.out.print("Both x and y values are false, which makes the NOR: ");
 			return (!(x || y));
-			}
+		} else {
+			System.out.print("Since the x value is " + x + " and the y value is " + y + ", this makes"
+					+ "the NOR value: ");
+			return (!(x || y));
+		}
 	}
 }
