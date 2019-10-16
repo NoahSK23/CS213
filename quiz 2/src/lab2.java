d5c38b0cdd4e03cc1115b7dbacfb051d483bfd29
NoahSK23 <noahsklinger@gmail.com>
2019-10-16 14:58:04 -0400

    New Version

--- quiz 2/src/lab2.java
+++ quiz 2/src/lab2.java
@@ -4,59 +4,59 @@ import java.util.LinkedList;
 public class lab2 {
 
 	public static void main(String[] args) {
-		Deque<Integer> d = new LinkedList<Integer>();
+		Deque<Integer> deque = new LinkedList<Integer>();
 		
-		Add(d);
-		System.out.println("This is your queue: " + d + "\nWe will remove Katherine and Tim");
-		System.out.println("The initial size is: " + size(d));
+		Add(deque);
+		System.out.println("This is your queue: " + deque + "\nWe will remove 1 and 7");
+		System.out.println("The initial size is: " + size(deque));
 		System.out.println("After the top and bottom elements are removed, there will be only 5");
-		System.out.println(pollFirst(d));
-		System.out.println(pollLast(d));
-		System.out.println("The size of the queue is now: " + d + " with a size of: " + size(d));
+		System.out.println(pollFirst(deque));
+		System.out.println(pollLast(deque));
+		System.out.println("The size of the queue is now: " + deque + " with a size of: " + size(deque));
 
 	}
 	
-	public static Deque<Integer> Add(Deque<Integer> d) {
-		d.add(4); // adds to queue
-		d.addFirst(3); // adds to front
-		d.addLast(5); // adds to end
-		d.push(2); // add to head
-		d.offer(6); // adds to queue
-		d.offerFirst(1); // adds to front
-		d.offerLast(7); // adds to back
+	public static Deque<Integer> Add(Deque<Integer> deque) {
+		deque.add(4); // adds to deque
+		deque.addFirst(3); // adds to front of the deque
+		deque.addLast(5); // adds to end of the deque
+		deque.push(2); // add to head of the deque
+		deque.offer(6); // adds to deque
+		deque.offerFirst(1); // adds number to the front of the deque
+		deque.offerLast(7); //adds number to the back of the deque
 
-		return d;
+		return deque;
 	}
 	
-	public static Deque<Integer> pollFirst(Deque<Integer> d) {
-		System.out.println("\n" + "First element of the queue removed");
-		d.pollFirst();
-		return d;
+	public static Deque<Integer> pollFirst(Deque<Integer> deque) {
+		System.out.println("\n" + "First element of the Deque removed");
+		deque.pollFirst();
+		return deque;
 	}
 
-	public static Deque<Integer> pollLast(Deque<Integer> d) {
-		System.out.println("\n" + "Last element of the queue removed");
-		d.pollLast();
-		return d;
+	public static Deque<Integer> pollLast(Deque<Integer> deque) {
+		System.out.println("\n" + "Last element of the Deque removed");
+		deque.pollLast();
+		return deque;
 	}
 
-	public static int size(Deque<Integer> d) {
-		int size = d.size();
+	public static int size(Deque<Integer> deque) {
+		int size = deque.size();
 		return size;
 	}
 
-	public static boolean isEmpty(Deque<Integer> d) {
-		d.isEmpty();
-		return d.isEmpty();
+	public static boolean isEmpty(Deque<Integer> deque) {
+		deque.isEmpty();
+		return deque.isEmpty();
 	}
 
-	public static Integer head(Deque<Integer> d) {
-		Integer head = d.peek();
+	public static Integer head(Deque<Integer> deque) {
+		Integer head = deque.peek();
 		return head;
 	}
 
-	public static Integer tail(Deque<Integer> d) {
-		Integer tail = d.peekLast();
+	public static Integer tail(Deque<Integer> deque) {
+		Integer tail = deque.peekLast();
 		return tail;
 	}
 
